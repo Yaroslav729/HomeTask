effee46565159022dcad1e0cc622010bef36bed9
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-21 23:08:44 +0300

    TaskSix

---
+++ taskSix/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+Console.Write("Число чётное");
+else Console.Write("Число не чётное");
