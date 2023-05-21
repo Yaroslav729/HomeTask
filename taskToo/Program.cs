e2b75869ff769f3cd49043a8f93e2f57649f9923
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-21 21:10:46 +0300

    taskToo

--- taskToo/Program.cs
+++ taskToo/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a < b)
+Console.WriteLine(b);
+else if (a > b )
+Console.Write(a);
+else
+Console.Write("Числа равны");
